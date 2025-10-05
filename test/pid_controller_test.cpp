5c622e495e57bc54190f651e54053cb733144a1f
Grayson Gilbert <graysongilbert@gmail.com>
2025-10-05 12:42:16 -0400

    Task #6: Added PID controller tests
    This commit adds 4 unique test cases for the PIDController class in the test subdirectory. The associated CMakeLists.txt files were updated to reflect these changes.

---
+++ test/pid_controller_test.cpp
@@ -0,0 +1,140 @@
+#include <gtest/gtest.h>
+#include "pid_controller.hpp"
+
+
+// Declaring multiple test scenarios for the PIDController class
+
+/*=========== Pure Proportional Controller Test ===========
+
+Gains: kp = 1.0, ki = 0.0, kd = 0.0
+Time interval = 1.0
+Set Point = 10.0
+Measured Value = 8.0
+
+Solve for Proportional Output:
+Proportional Output -> Kp * error
+Error -> 10.0 - 8.0 = 2.0
+
+Proportional Output = 1.0 * 2.0 = 2.0
+
+*/
+TEST(PIDControllerTest1, ProportionalTest) {
+	PIDController controller(1.0, 0.0, 0.0, 1.0);
+	double output = controller.compute(10.0, 8.0);
+	EXPECT_DOUBLE_EQ(output, 2.0);
+}
+
+/*=========== Pure Integrator Test with Two Steps ===========
+
+- The purpose of the two steps in the test is to show that the integral error will accumulate between loops
+
+Gains: kp = 0.0, ki = 1.0, kd = 0.0
+Time interval = 1.0
+Set Point = 5.0
+Measured Value = 0.0
+Integral Error = 0.0
+
+=========== Solve for Integral Output of loop 1: ===========
+
+Integral Error -> Integral Error + Error
+Error -> 5.0 - 0.0 = 5.0
+Integral Output ->  Ki * Integral Error 
+Integral Output -> 1.0 * 5.0 = 5.0 
+
+=========== Solve for Integral Output of loop 2: ===========
+
+Integral Error = 5.0
+Error -> 5.0 - 0.0 = 5.0
+Integral Error -> 5.0 + 5.0 = 10.0
+Integral Output -> 1.0 * 10.0 = 10.0
+*/
+
+TEST(PIDControllerTest2, IntegralTest) {
+	PIDController controller(0.0, 1.0, 0.0, 1.0);
+	double first_output = controller.compute(5.0, 0.0);
+	EXPECT_DOUBLE_EQ(first_output, 5.0);
+
+	double second_output = controller.compute(5.0, 0.0);
+	EXPECT_DOUBLE_EQ(second_output, 10.0);
+
+}
+
+/*===========  Pure Derivative Test with Two Steps ===========
+
+Gains: kp = 0.0, ki = 0.0, kd = 1.0
+
+=========== Solve for Derivative Output of Loop 1: ===========
+
+Time interval = 1.0
+Set Point = 10.0
+Measured Value = 8.0
+Previous Error = 0.0
+Error -> 10.0 - 8.0 = 2.0
+
+Derivative Output -> kd * (error - previous error) / dt
+Derivative Output = 1.0 * (2.0 - 0.0) / 1.0 = 2.0
+
+=========== Solve for Derivative Output of Loop 2: ===========
+
+Set Point = 10.0
+Measured Value = 9.0
+Previous Error = 2.0
+Error -> 10.0 - 9.0 = 1.0
+
+Derivative Output -> 1.0 * (1.0 - 2.0) / 1 = -1
+*/
+TEST(PIDControllerTest3, DerivativeTest) {
+	PIDController controller(0.0, 0.0, 1.0, 1.0);
+	double first_output = controller.compute(10.0, 8.0);
+	EXPECT_DOUBLE_EQ(first_output, 2.0);
+
+	double second_output = controller.compute(10.0, 9.0);
+	EXPECT_DOUBLE_EQ(second_output, -1.0);
+}
+
+/* =========== Mixed PID Test ===========
+
+Gains: kp = 1.0, ki = 0.5, kd = 0.0
+Set Point = 4.0
+Measured Value = 2.0
+Time Interval = 1.0
+Integral Error = 0.0
+Previous Error = 0.0
+
+Proportional Output -> Kp * error
+Integral Error -> Integral Error + Error
+Integral Output ->  Ki * Integral Error 
+Derivative Output -> kd * (error - previous error) / dt
+
+PID Output -> Proportional Output + Integral Output + Derivative Output
+
+=========== Solving for PID Output of Loop 1: ===========
+
+Error -> 4.0 - 2.0 = 2.0
+Proportional Output -> 1.0 * 2.0 = 2.0
+Integral Error -> 0.0 + 2.0 = 2.0
+Integral Output -> 0.5 * 2.0 = 1.0
+Derivative Output -> 0.0 * (2.0 - 0.0) / 1.0 = 0.0
+
+PID Output = 2.0 + 1.0 + 0.0 = 3.0
+
+=========== Solving for PID Output of Loop 2: ===========
+
+Error = 4.0 - 2.0 = 2.0
+Proportional Output -> 1.0 * 2.0 = 2.0
+Integral Error -> 2.0 + 2.0 = 4.0
+Integral Output -> 0.5 * 4.0 = 2.0
+Derivative Output -> 0.0 * (2.0 - 2.0) / 1.0 = 0.0
+
+PID Output -> 2.0 + 2.0 + 0.0 = 4.0
+*/
+
+TEST(PIDControllerTest4, FullPIDTest) {
+	PIDController controller(1.0, 0.5, 0.0, 1.0);
+	double first_output = controller.compute(4.0, 2.0);
+	EXPECT_DOUBLE_EQ(first_output, 3.0);
+
+	double second_output = controller.compute(4.0, 2.0);
+	EXPECT_DOUBLE_EQ(second_output, 4.0);
+
+}
--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(dummy_test, this_should_pass) {
-  EXPECT_EQ(1, 1);
-}
-
